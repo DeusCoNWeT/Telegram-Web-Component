477b47d70b179f17c7d803d1f17f80d34a88c39d
Javier Caballero <javier.caballero.abenza@gmail.com>
2016-12-02 11:51:23 +0100

    Server dependencies + auth code sending

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
+var express = require('express');
 var telegramLink = require('telegram.link')();
+var app = express();
 
+//node_modules
+app.use(express.static('node_modules'));
+
+app.listen(3000);
+
+/* Connect client (not user) to telegram servers */
 var connected = function(ex){
   if(!ex) { 
     console.log('Connected to Telegram!'); 
@@ -9,11 +17,13 @@ var connected = function(ex){
 
 var authKey = null;
 
+/* Create the auth key to communicate with server */
 var keyCreated = function(response){
   if(response.error) { console.log('Fucked up: ',error); return }
   authKey = response.key;
   console.log('Got the key: ',authKey);
 
+  /* Send the auth code to the user phone */
   client.auth.sendCode(
   '34625355635',
   5,
@@ -22,12 +32,14 @@ var keyCreated = function(response){
   );
 }
 
+/* Get the code hash to sing in a user */
 var codeResponse = function(response){
   console.log('Code response: ',response);
 }
 
+/* Connect client (not user) to telegram servers */
 var client = telegramLink.createClient(
-  {
+  {/* TEST id and hash change to your app credentials on deploy */
     id: 10534,
     hash: '844584f2b1fd2daecee726166dcc1ef8',
     version: '1.0.0',
